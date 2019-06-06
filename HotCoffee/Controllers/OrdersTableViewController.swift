31cc7caab3df67a46bfff7688080a13898612f93
Lazar Popovic <lazar.popovic.m@gmail.com>
2019-06-06 15:18:45 +0200

    make first api call

--- HotCoffee/Controllers/OrdersTableViewController.swift
+++ HotCoffee/Controllers/OrdersTableViewController.swift
@@ -10,4 +10,28 @@ import UIKit
 
 class OrdersTableViewController: UITableViewController {
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        populateOrders()
+    }
+    
+    private func populateOrders() {
+        
+        guard  let coffeeordersURL = URL(string: "https://guarded-retreat-82533.herokuapp.com/orders") else {
+            fatalError("URL was incorrect")
+        }
+        
+        let resource = Resource<[Order]>(url: coffeeordersURL)
+        
+        WebService().load(resource: resource) { (result) in
+            
+            switch result {
+            case .success(let orders):
+                print(orders)
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
+    }
 }
