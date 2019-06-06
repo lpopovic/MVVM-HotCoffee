518678ade6ed8983f471eb43616d598ff0829823
Lazar Popovic <lazar.popovic.m@gmail.com>
2019-06-06 14:38:41 +0200

    init web service

---
+++ HotCoffee/Services/WebService.swift
@@ -0,0 +1,46 @@
+//
+//  WebService.swift
+//  HotCoffee
+//
+//  Created by MacBook Air on 6/6/19.
+//  Copyright © 2019 Riitech. All rights reserved.
+//
+
+import Foundation
+
+struct Resource<T: Codable> {
+    let url: URL
+}
+
+enum NetworkError: Error{
+    case decodingError
+    case domainError
+    case urlError
+}
+
+class WebService {
+    
+    func load<T>(resource: Resource<T>, completion: @escaping (Result<T,NetworkError>)->Void) {
+       
+        URLSession.shared.dataTask(with: resource.url) { (data, response, error) in
+            
+            guard let data = data, error == nil else {
+                
+                completion(.failure(.domainError))
+                
+                return
+            }
+            
+            let result = try? JSONDecoder().decode(T.self, from: data)
+            if let result = result {
+                DispatchQueue.main.async {
+                    completion(.success(result))
+                }
+            }else {
+                completion(.failure(.decodingError))
+            }
+            
+        }.resume()
+        
+    }
+}
