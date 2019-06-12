0ee74d45642338ae9ca5c053951e204c5feee3fe
Lazar Popovic <lazar.popovic.m@gmail.com>
2019-06-12 15:35:55 +0200

    START Populating AddCoffeeOrderViewModel

--- HotCoffee/Controllers/AddOrderViewController.swift
+++ HotCoffee/Controllers/AddOrderViewController.swift
@@ -10,4 +10,53 @@ import UIKit
 
 class AddOrderViewController: UIViewController {
     
+    private var vm = AddCoffeOrderViewModel()
+    
+    @IBOutlet weak var tableView: UITableView!
+    private var coffeSizesSegmentedControl: UISegmentedControl!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.setupUI()
+        
+    }
+    
+    private func setupUI() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        self.coffeSizesSegmentedControl = UISegmentedControl(items: vm.sizes)
+        self.coffeSizesSegmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(self.coffeSizesSegmentedControl)
+        self.coffeSizesSegmentedControl.topAnchor.constraint(equalTo: self.tableView.bottomAnchor, constant: 20).isActive = true
+         self.coffeSizesSegmentedControl.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+    }
+}
+extension AddOrderViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return vm.types.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CoffeTypeTableViewCell", for: indexPath)
+        
+        cell.textLabel?.text = vm.types[indexPath.row]
+        return cell
+    }
+}
+extension AddOrderViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        tableView.cellForRow(at: indexPath)?.accessoryType = .none
+    }
 }
--- HotCoffee/Models/Order.swift
+++ HotCoffee/Models/Order.swift
@@ -8,14 +8,14 @@
 
 import Foundation
 
-enum CoffeType: String, Codable {
+enum CoffeType: String, Codable, CaseIterable {
     case cappuccino
     case latte
     case espressino
     case cortado
 }
 
-enum CoffeSize: String, Codable {
+enum CoffeSize: String, Codable, CaseIterable {
     case small
     case medium
     case large
@@ -23,7 +23,6 @@ enum CoffeSize: String, Codable {
 
 
 struct Order: Codable {
-    
     let name: String
     let email: String
     let type: CoffeType
